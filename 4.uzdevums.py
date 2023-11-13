eea0560abed41558c8ba4602f93e5e209d1ecb8d
RobertsGK <148037449+Roberts-GK@users.noreply.github.com>
2023-11-13 10:17:20 +0200

    Create 4.uzdevums.py

---
+++ 4.uzdevums.py
@@ -0,0 +1,8 @@
+skaitlis = int(input("Ievadi veselu pozitivu skaitli:"))
+
+faktorials = 2
+
+for i in range(2, skaitlis +1):
+    faktorials = i
+
+print(f"Faktroials no {skaitlis} ir: {faktorials}")
